ef403ca4756059755bdfb73dc870bb106daf0ec8
rahuldhadvai <rahul.dhadvai1@gmail.com>
2018-06-14 14:54:15 +0530

    Update create_table.php

--- create_table.php
+++ create_table.php
@@ -16,7 +16,7 @@ echo "Connected to Database successfully";
 
 // Create a table in the database
 
-$table = "create table app_jsons_demo(
+$table = "create table app_jsons(
 	
 	app_name varchar(30) NOT NULL PRIMARY KEY,
 	json_string varchar(10000)
